7de448e99c9b0eb7275ad99317f1afa26283c6ff
Jeremy Gebben <jeremyg@lunarg.com>
2021-03-04 09:41:54 -0700

    [WIP] layers: Use robin_hood unordered map and set
    Change-Id: Ib5a2b4c3c9554f8fe5c67c20d7e29e89a83fc4f3

--- layers/layer_data_types.h
+++ layers/layer_data_types.h
@@ -1,11 +1,10 @@
 #pragma once
-#include <unordered_map>
-#include <unordered_set>
+#include "robin_hood.h"
 
 namespace layers {
-template <typename Key, typename Hash = std::hash<Key>, typename KeyEqual = std::equal_to<Key> >
-using unordered_set = std::unordered_set<Key, Hash, KeyEqual>;
+template <typename Key, typename Hash = robin_hood::hash<Key>, typename KeyEqual = std::equal_to<Key> >
+using unordered_set = robin_hood::unordered_set<Key, Hash, KeyEqual>;
 
-template <typename Key, typename T, typename Hash = std::hash<Key>, typename KeyEqual = std::equal_to<Key> >
-using unordered_map = std::unordered_map<Key, T, Hash, KeyEqual>;
+template <typename Key, typename T, typename Hash = robin_hood::hash<Key>, typename KeyEqual = std::equal_to<Key> >
+using unordered_map = robin_hood::unordered_map<Key, T, Hash, KeyEqual>;
 };
